9358ce5957471f113ae5568c0a18bf9750ea748b
Dennis van Ginkel <d.vanginkel@horizoncollege.nl>
2015-09-09 14:05:59 +0200

    - Magazijn In-Uitvoer form - CSV import voor klanten En nog wat kleine updates

---
+++ Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_inoutlist.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Helpdesk_Registratie_Tool_Purmerend.Magazijn
+{
+    partial class magazijn_inoutlist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(magazijn_inoutlist));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(808, 241);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // magazijn_inoutlist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 474);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "magazijn_inoutlist";
+            this.Text = "Magazijn In- Uitvoer Overzicht";
+            this.Load += new System.EventHandler(this.magazijn_inoutlist_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_inoutlist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helpdesk_Registratie_Tool_Purmerend.Magazijn
+{
+    public partial class magazijn_inoutlist : Form
+    {
+        public magazijn_inoutlist()
+        {
+            InitializeComponent();
+        }
+
+        private void magazijn_inoutlist_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton12_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
