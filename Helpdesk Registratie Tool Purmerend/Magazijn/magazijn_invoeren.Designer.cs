f20cdcca06666e415965a6b1ee12a5080405346d
Dennis van Ginkel <d.vanginkel@horizoncollege.nl>
2015-09-14 09:42:49 +0200

    Magazijn Invoeren coderen.

--- Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_invoeren.Designer.cs
+++ Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_invoeren.Designer.cs
@@ -30,15 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(magazijn_invoeren));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.magazijn_invoer_combobx_subcategorie = new System.Windows.Forms.ComboBox();
             this.magazijn_invoer_combobx_categorie = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.magazijn_invoer_combobx_subcategorie);
             this.groupBox1.Controls.Add(this.magazijn_invoer_combobx_categorie);
             this.groupBox1.Controls.Add(this.label1);
@@ -48,6 +59,39 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Product Invoeren";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fabrikant";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Tag = "Hardeschijf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Product";
             // 
             // magazijn_invoer_combobx_subcategorie
             // 
@@ -90,11 +134,30 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(153, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Sub categorie";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label6";
+            // 
             // magazijn_invoeren
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 481);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -115,5 +178,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox magazijn_invoer_combobx_subcategorie;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_invoeren.cs
+++ Helpdesk Registratie Tool Purmerend/Magazijn/magazijn_invoeren.cs
@@ -16,6 +16,7 @@ namespace Helpdesk_Registratie_Tool_Purmerend.Magazijn
         {
             try
             {
+
                 SqlConnection conn = new SqlConnection(@"Data Source=HELPDESK-PC\SQLEXPRESS;Initial Catalog=helpdesk;Integrated Security=True");
                 conn.Open();
                 SqlCommand sc = new SqlCommand("SELECT ID, categorie from magazijncategorieen", conn);
@@ -33,17 +34,18 @@ namespace Helpdesk_Registratie_Tool_Purmerend.Magazijn
 
                 conn.Close();
 
+
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Oeps zomer: \n" + ex);
+                MessageBox.Show("Er ging iets fout : \n" + ex);
             }
         }
 
         private void magazijn_invoer_combobx_categorie_SelectedIndexChanged(object sender, EventArgs e)
         {
             // ID van geselecteerde waarde weergeven in Label2
-            label2.Text = "Huidige ID :" + magazijn_invoer_combobx_categorie.SelectedValue.ToString();
+            label2.Text = "Huidige ID : " + magazijn_invoer_combobx_categorie.SelectedValue.ToString();
             magazijn_invoer_combobx_subcategorie.Items.Clear();
             magazijn_invoer_combobx_subcategorie.Visible = true;
             magazijn_invoer_combobx_subcategorie.Show();
@@ -56,15 +58,40 @@ namespace Helpdesk_Registratie_Tool_Purmerend.Magazijn
 
             reader = sc.ExecuteReader();
             DataTable dt = new DataTable();
+
             dt.Columns.Add("subcategorie", typeof(string));
             dt.Columns.Add("subcategorie1", typeof(string));
+            dt.Columns.Add("subcategorie2", typeof(string));
+            dt.Columns.Add("subcategorie3", typeof(string));
+            dt.Columns.Add("subcategorie4", typeof(string));
+            dt.Columns.Add("subcategorie5", typeof(string));
+            dt.Columns.Add("subcategorie6", typeof(string));
+            dt.Columns.Add("subcategorie7", typeof(string));
+            dt.Columns.Add("subcategorie8", typeof(string));
+            dt.Columns.Add("subcategorie9", typeof(string));
+            dt.Columns.Add("subcategorie10", typeof(string));
+            dt.Columns.Add("subcategorie11", typeof(string));
+
             dt.Load(reader);
 
-            magazijn_invoer_combobx_subcategorie.ValueMember = "subcategorie";
-            magazijn_invoer_combobx_subcategorie.DisplayMember = "subcategorie";
-            magazijn_invoer_combobx_subcategorie.DataSource = dt;
+            foreach (DataRow row in dt.Rows)
+            {
 
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie1"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie2"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie3"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie4"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie5"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie6"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie7"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie8"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie9"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie10"].ToString());
+                magazijn_invoer_combobx_subcategorie.Items.Add(row["subcategorie11"].ToString());
+            }
             conn.Close();
+
         }
 
     }
